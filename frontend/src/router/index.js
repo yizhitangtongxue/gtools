8ab03a6f997afba8fcf0a52cbb8653037261fee4
yizhitangtongxue <yizhitangtongxue@gmail.com>
2022-11-29 10:52:25 +0800

    add route

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routers = [
+    {
+        "path":"/",
+        "name":"Home",
+        "component":()=>import("../components/PortList.vue")
+    },
+    {
+      "path":"/hello",
+      "name":"HelloWorld",
+      "component":()=>import("../components/HelloWorld.vue")
+  }
+  ]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routers,
+});
+
+export default router;
+
