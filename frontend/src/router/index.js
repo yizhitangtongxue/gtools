0fd8e05c3aedfcf185df5367a89f103df7d51cae
yizhitangtongxue <yizhitangtongxue@gmail.com>
2022-11-30 15:51:23 +0800

    remove vue-codemirror

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routers = [
-    {
-        "path":"/",
-        "name":"Home",
-        "component":()=>import("../components/PortList.vue")
-    },
-    {
-      "path":"/hello",
-      "name":"HelloWorld",
-      "component":()=>import("../components/HelloWorld.vue")
+  {
+    "path": "/",
+    "name": "Home",
+    "component": () => import("../components/PortList.vue")
+  },
+  {
+    "path": "/hello",
+    "name": "HelloWorld",
+    "component": () => import("../components/HelloWorld.vue")
+  },
+  {
+    "path": "/mavendld",
+    "name": "MavenDld",
+    "component": () => import("../components/MavenDld.vue")
   }
-  ]
+]
 
 const router = createRouter({
   history: createWebHistory(),
